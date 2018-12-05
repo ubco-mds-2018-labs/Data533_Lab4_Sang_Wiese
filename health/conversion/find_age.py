282dfbb4b22d7c2a5cd62c4d9c0ab4a59b9ba69e
April. Yueyue Sang <42953515+aprilsang47@users.noreply.github.com>
2018-12-05 15:12:19 -0800

    Update find_age.py

--- health/conversion/find_age.py
+++ health/conversion/find_age.py
@@ -4,9 +4,12 @@ from datetime import datetime
 def year_to_age(name,bdate): #enter a string in form yyyy-mm-dd
     today = datetime.now()
     # Calculate age
-    dt = datetime.date(datetime.strptime(bdate, "%Y-%m-%d"))
-    age = today.year - dt.year
-    if today.month < dt.month or (today.month == dt.month and today.day < dt.day):
-        age = age - 1   # Handle case when have not had birthday yet
-    #print("%s Age: %s" %(name,age))
-    return age
+    try:
+        dt = datetime.date(datetime.strptime(bdate, "%Y-%m-%d"))
+        age = today.year - dt.year
+        return age
+    except: 
+        print("Invaild DOB format!")
+
+a1 = Swimmer('Kim','1987-04',76,178,75,68)
+year_to_age(a1.name,a1.DOB)
