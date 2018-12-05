b4bfb03e82019e4a6e6c049a18001d5b809fe563
April. Yueyue Sang <42953515+aprilsang47@users.noreply.github.com>
2018-12-05 15:15:28 -0800

    Update predictparents.py

--- health/prediction/predictparents.py
+++ health/prediction/predictparents.py
@@ -1,4 +1,11 @@
 def Predicted_height(name, fatherheight, motherheight):
-        PredictedHeight = (fatherheight+motherheight)/2
-        #print("%s 's predicted height: %s" % (name,PredictedHeight))
+    try:
+        if fatherheight > 0 and motherheight > 0:
+            PredictedHeight = (fatherheight+motherheight)/2
+        #print("%s 's predicted height: %s" % (name,PredictedHeight)) 
         return PredictedHeight
+    except: 
+        print("Error,Missing Values in parents' height!")
+
+a1 = Swimmer('Kim','1987-04-01',76,178,0,68)
+Predicted_height(a1.name,a1.fatherheight,a1.motherheight)
