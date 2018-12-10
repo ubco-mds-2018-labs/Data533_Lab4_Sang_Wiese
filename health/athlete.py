db3c5dc510bbd9b1174ab08a30477388bd5db4b2
April. Yueyue Sang <42953515+aprilsang47@users.noreply.github.com>
2018-12-10 15:08:16 -0800

    Update athlete.py

--- health/athlete.py
+++ health/athlete.py
@@ -7,15 +7,17 @@ class athlete():
 class Swimmer(athlete): # Swimmer subclass, represents a swimmer
     def __init__(self, name, DOB, height, weight, fatherheight, motherheight):
         athlete.__init__(self, name, DOB)
-        self.height = int(height)
-        self.weight = int(weight)
+        self.height = height
+        self.weight = weight
+        self.fatherheight = fatherheight
+        self.motherheight = motherheight
         print('(Initialized Swimmer: {})'.format(self.name))
-
 
 class Powerlifter(athlete):
     def __init__(self, name, DOB, height, weight, fatherheight, motherheight):
         athlete.__init__(self, name, DOB)
         self.height = height
         self.weight = weight
+        self.fatherheight = fatherheight
+        self.motherheight = motherheight
         print('(Initialized powerlifter: {})'.format(self.name))
-
