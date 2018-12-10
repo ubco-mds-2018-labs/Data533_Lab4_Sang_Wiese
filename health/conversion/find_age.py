bc00b13249cbb53f15b79f40b373f9a77cf12a1e
Kira Wiese <42852879+kirawiese@users.noreply.github.com>
2018-12-10 15:19:49 -0800

    Update find_age.py

--- health/conversion/find_age.py
+++ health/conversion/find_age.py
@@ -7,6 +7,8 @@ def year_to_age(name,bdate): #enter a string in form yyyy-mm-dd
     try:
         dt = datetime.date(datetime.strptime(bdate, "%Y-%m-%d"))
         age = today.year - dt.year
+        if today.month < dt.month or (today.month == dt.month and today.day < dt.day):
+            age = age - 1
         return age
     except ValueError: 
         print("Please enter the date of birth as yyyy-mm-dd.")
